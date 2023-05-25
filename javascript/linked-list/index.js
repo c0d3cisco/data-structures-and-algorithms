73199309531af57bec65ad335249ac1fc8a55e02
Francisco Sanchez <c0d3cisco@yahoo.com>
2023-05-24 23:39:31 -0500

    set up LinkedList and finished code for code challenge 08

--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,7 +1,151 @@
 'use strict';
 
-class LinkedList {
-
+class Node {
+  //Think of Node as the box itself. It's content and handling instruction are required at a minimum. Additional properties could be added.
+  constructor(data, next = null){
+    this.data = data;
+    this.next = next;
+  }
 }
 
-module.exports = LinkedList;
+class LinkedList {
+  //Think of LinkedList supply chain. The List can be equipped with additional methods to execute its purposes.
+  constructor(){
+    this.head = null; // set to null by default
+    this.size = 0;
+  }
+
+  // * This are different methods for the Linked list
+  // insert first node
+  insertFirst(data) {
+    // * Below creates the new node by adding the data and assigning the next head with the current head (which is null when inserting the first {100} -> null)
+    this.head = new Node(data, this.head);
+    this.size++;
+  }
+  // insert last node
+  insertLast(data){
+    let node = new Node(data);
+    let current;
+
+    // If empty, make head
+    if(!this.head){
+      this.head = node;
+    } else {
+      current = this.head;
+
+      while(current.next) {
+        current = current.next;
+      }
+
+      current.next = node;
+    }
+
+    this.size++;
+  }
+  // insert at index
+  insertAt(data, index){
+
+    // If index is out of range
+    if(index > 0 && index > this.size){
+      return;
+    }
+
+    // if first index
+    if(index === 0){
+      this.insertFirst(data);
+      return;
+    }
+
+    const node = new Node(data);
+    let current, previous;
+
+    // set curernt to first
+    current = this.head;
+    let count = 0;
+
+    while(count < index) {
+      previous = current; // node before index
+      count++;
+      current = current.next; // Node after index
+    }
+
+    node.next = current;
+    previous.next = node;
+
+    this.size++;
+
+  }
+  // get at index
+  getAt(index){
+    let current = this.head;
+    let count = 0;
+
+    while(current){
+      if(count === index) {
+        console.log(current.data);
+      }
+      count++;
+      current = current.next;
+    }
+
+    return null;
+  }
+  // remove at index
+  removeAt(index){
+    if( index> 0 && index > this.size) {
+      return;
+    }
+
+    let current = this.head;
+    let previous;
+    let count = 0;
+
+    // Remove first
+    if(index === 0){
+      this.head = current.next;
+    } else {
+      while( count < index ){
+        count++;
+        previous = current;
+        current = current.next;
+      }
+
+      previous.next = current.next;
+    }
+
+    this.size--;
+  }
+  // clear list
+  clearList() {
+    this.head = null;
+    this.size = 0;
+  }
+  // print list data
+  printListData() {
+    let current = this.head;
+
+    while(current) {
+      console.log(current.data);
+      current = current.next;
+    }
+  }
+}
+
+const zipLists = (list1, list2) => {
+  let current1 = list1.head;
+  let current2 = list2.head;
+  const newList = new LinkedList();
+  while (current1 || current2) {
+    if (current1) {
+      newList.append(current1.value);
+      current1 = current1.next;
+    }
+    if (current2) {
+      newList.append(current2.value);
+      current2 = current2.next;
+    }
+  }
+  return newList;
+};
+
+module.exports = { LinkedList, zipLists };
